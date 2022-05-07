c224fc44236130f0cc1e60421c5d5e4d01609b13
Daniil Lebedev <93409280+holu31@users.noreply.github.com>
2022-05-07 22:27:57 +0300

    delete putchar syscall

--- kernel/include/interrupts/syscalls.h
+++ kernel/include/interrupts/syscalls.h
@@ -7,7 +7,6 @@
 #define SC_CODE_gets            3
 #define SC_CODE_malloc          4
 #define SC_CODE_free            5
-#define SC_CODE_putchar         6
 #define SC_CODE_putpixel        32
 #define SC_CODE_drawline        33
 #define SC_CODE_version         40
