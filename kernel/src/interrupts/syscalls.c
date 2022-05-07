0eeae0d40982abcd90335fa8a769d89e7b38c3fe
Daniil Lebedev <93409280+holu31@users.noreply.github.com>
2022-05-07 22:15:19 +0300

    Update registr result in syscalls

--- kernel/src/interrupts/syscalls.c
+++ kernel/src/interrupts/syscalls.c
@@ -10,39 +10,39 @@ void syscall_init() {
 void syscall_handler(struct regs *r) {
     switch (r->eax) {
         case SC_CODE_puts:
-            tty_printf("%s", (char*) ((uint32_t*)(r->ebx)));
-            r->edx = (uint32_t)1;
+            tty_printf("%s", (char*) ((r->ebx)));
+            r->eax = (uint32_t)1;
             break;
         case SC_CODE_getscancode:
-            r->edx = (uint32_t)keyboard_getscancode();
+            r->eax = (uint32_t)keyboard_getscancode();
             break;
         case SC_CODE_getchar:
-            r->edx = (uint32_t)keyboard_getchar();
+            r->eax = (uint32_t)keyboard_getchar();
             break;
         case SC_CODE_gets:
-            r->edx = (uint32_t)keyboard_gets();
+            r->eax = (uint32_t)keyboard_gets();
             break;
         case SC_CODE_malloc:
-            r->edx = (uint32_t)kheap_malloc((int)(r->ebx));
+            r->eax = (uint32_t)kheap_malloc((int)(r->ebx));
             break;
         case SC_CODE_free:
             kheap_free((void*)(r->ebx));
-            r->edx = (uint32_t)1;
+            r->eax = (uint32_t)1;
             break;
         case SC_CODE_putchar:
             tty_putchar((char) ((uint32_t*)(r->ebx)));
-            r->edx = (uint32_t)1;
+            r->eax = (uint32_t)1;
             break;
         case SC_CODE_putpixel:
-            set_pixel((int) ((uint32_t*) (r->ebx)), (int) ((uint32_t*) (r->ecx)), (uint32_t)((uint32_t*) (r->edx)));
-            r->edx = (uint32_t)1;
+            set_pixel((int) ((uint32_t*) (r->ebx)), (int) ((uint32_t*) (r->ecx)), 0xAA00AA);
+            r->eax = (uint32_t)1;
             break;
         case SC_CODE_drawline:
             set_line((int) ((uint32_t*) (r->ebx)), (int) ((uint32_t*) (r->ecx)),(int) ((uint32_t*) (r->edx)), (int) ((uint32_t*) (r->esi)), (uint32_t) ((uint32_t*) (r->edi)));
-            r->edx = (uint32_t)1;
+            r->eax = (uint32_t)1;
             break;
         case SC_CODE_version:
-            r->edx = (uint32_t)(VERSION_MAJOR * 100 + VERSION_MINOR);
+            r->eax = (uint32_t)(VERSION_MAJOR * 100 + VERSION_MINOR);
             break;
         default: 
             qemu_printf("Invalid syscall #%x\n", r->eax);
