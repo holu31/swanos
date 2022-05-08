8f08577f60717d46eea9e58f35cac9fe672c1913
Daniil Lebedev <93409280+holu31@users.noreply.github.com>
2022-05-08 17:22:19 +0300

    second optimization

--- gcc/libc/stdio.c
+++ gcc/libc/stdio.c
@@ -1,44 +1,36 @@
 #include <stdio.h>
 
 int puts(char *str){
-    asm volatile("int $0x80" ::"a"(0), "b"(str));
-    return 0;
+    int result = -1;
+
+    asm volatile("int $0x80" : "=a"(result) : "a"(0), "b"(str));
+    return result;
 }
 
 int getscancode(){
     int result = -1;
 
-    asm volatile("int $0x80" ::"a"(1));
-    asm volatile("mov %%eax, %0" : "=a"(result));
-
+    asm volatile("int $0x80" : "=a"(result) : "a"(1));
     return result;
 }
 
 char getchar(){
     int result = -1;
 
-    asm volatile("int $0x80" ::"a"(2));
-    asm volatile("mov %%eax, %0" : "=a"(result));
-
+    asm volatile("int $0x80" : "=a"(result) : "a"(2));
     return result;
 } 
 
 char *gets() {
-    void* res = 0;
     char *result = "";
 
-    asm volatile("int $0x80" ::"a"(3));
-    asm volatile("mov %%eax, %0" : "=a"(result));
-
+    asm volatile("int $0x80" : "=a"(result) : "a"(3));
     return result;
 } 
 
 int getversion(){
     int result = -1;
-    
 
-    asm volatile("int $0x80" ::"a"(4));
-    asm volatile("mov %%eax, %0" : "=a"(result));
-
+    asm volatile("int $0x80" : "=a"(result) :"a"(4));
     return result;
 }
