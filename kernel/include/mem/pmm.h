2ba9785f99b246c2ff326297f20a68453f0740ff
Daniil Lebedev <93409280+holu31@users.noreply.github.com>
2022-05-20 19:18:11 +0300

    get rid of the synapse

--- kernel/include/mem/pmm.h
+++
@@ -1,4 +0,0 @@
-#include <mem/mem.h>
-
-uint64_t phys_installed_memory_size, phys_available_memory_size;
-void pmm_init(struct multiboot_info *mboot_info);
--- kernel/src/drivers/time.c
+++
@@ -1,7 +0,0 @@
-#include <kernel.h>
-
-
-void timer_set_frequency(int i){
-    i = 0;
-    return (void)i;
-}
