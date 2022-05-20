1bfad29e0e0d12521deefb7a128ae138533d48ef
Daniil Lebedev <93409280+holu31@users.noreply.github.com>
2022-05-20 19:58:43 +0300

    compiler fix due to amendment

--- build.py
+++ build.py
@@ -2,11 +2,14 @@ import os, sys
 
 OBJECTS = "bin/kernel.o bin/kernel_entry.o bin/console.o bin/ports.o bin/string.o bin/gdt.o bin/idt.o"
 
-os.system('i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/libc/string.c -o bin/string.o')
 os.system('i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/io/ports.c -o bin/ports.o')
 os.system('i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/io/console.c -o bin/console.o')
-os.system('i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/interrupts/gdt.c -o bin/gdt.o')
-os.system('i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/interrupts/idt.c -o bin/idt.o')
+
+os.system('i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/libc/string.c -o bin/string.o')
+
+os.system('i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/arch/x86/gdt.c -o bin/gdt.o')
+os.system('i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/arch/x86/idt.c -o bin/idt.o')
+
 os.system('i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/kernel.c -o bin/kernel.o')
 
 os.system('i686-elf-as src/kernel_entry.s -o bin/kernel_entry.o')
