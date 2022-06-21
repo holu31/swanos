87866fb5adc103c1dd0807e4aa618e443dbcdabf
Daniil Lebedev <93409280+holu31@users.noreply.github.com>
2022-06-21 18:06:01 +0300

    update linker, add enable pading(for memory manager)

--- src/kernel_entry.s
+++ src/kernel_entry.s
@@ -126,6 +126,26 @@ gdt_flush:
 
 .flush:
     ret # back to c
+
+.global enable_paging
+
+enable_paging:
+    # Переместить каталог в CR3
+    mov 4(%esp), %eax
+    #mov (0x00009000), %eax
+    mov %eax, %cr3
+
+    # Отключаем 4х мегабайтые страницы
+    mov %cr4, %ecx
+    and $~0x00000010, %ecx
+    mov %ecx, %cr4
+
+    # Включаем пейджинг
+    mov %cr0, %eax
+    or $0x80000000, %eax
+    mov %eax, %cr0
+    ret
+    
 .skip_idt:
 
 4:
