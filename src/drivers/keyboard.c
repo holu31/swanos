fdf75970d928d65d90d3f0078c0c101539445941
Daniil Lebedev <93409280+holu31@users.noreply.github.com>
2022-06-22 17:43:24 +0300

    update keyboard driver

--- src/drivers/keyboard.c
+++ src/drivers/keyboard.c
@@ -1,54 +1,33 @@
 #include <keyboard.h>
 #include <ports.h>
 
-char kb_[128] = {
-    0,  27, '1', '2', '3', '4', '5', '6', '7', '8', /* 9 */
-    '9', '0', '-', '=', '\b',   /* Backspace */
-    '\t',           /* Tab */
-    'q', 'w', 'e', 'r', /* 19 */
-    't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',       /* Enter key */
-    0,          /* 29   - Control */
-    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',   /* 39 */
-    '\'', '`',   0,     /* Left shift */
-    '\\', 'z', 'x', 'c', 'v', 'b', 'n',         /* 49 */
-    'm', ',', '.', '/',   0,                    /* Right shift */
-    '*',
-    0,  /* Alt */
-    ' ',    /* Space bar */
-    0,  /* Caps lock */
-    0,  /* 59 - F1 key ... > */
-    0,   0,   0,   0,   0,   0,   0,   0,
-    0,  /* < ... F10 */
-    0,  /* 69 - Num lock*/
-    0,  /* Scroll Lock */
-    0,  /* Home key */
-    0,  /* Up Arrow */
-    0,  /* Page Up */
-    '-',
-    0,  /* Left Arrow */
-    0,
-    0,  /* Right Arrow */
-    '+',
-    0,  /* 79 - End key*/
-    0,  /* Down Arrow */
-    0,  /* Page Down */
-    0,  /* Insert Key */
-    0,  /* Delete Key */
-    0,   0,   0,
-    0,  /* F11 Key */
-    0,  /* F12 Key */
-    0,  /* All other keys are undefined */
-};
-
-uint8_t kb_getkey(){
-    if((inb(0x64) & 1) != 0) return inb(KB_DATA_PORT);
+int kb_getkey(){
+    if(inb(0x64) & 1) return inb(KB_DATA_PORT);
     return 0;
 }
 
 char kb_getchar(){
-    return kb_[kb_getkey()];
+    return kb_sctochar(kb_getkey);
 }
 
 char kb_sctochar(int scancode){     // scancode to char
-    return kb_[scancode];
+    if(scancode == 0x1C) return '\n';
+	if(scancode == 0x39) return ' ';
+	if(scancode == 0xE) return '\r';
+	if(scancode == 0xB4) return '.';
+	if(scancode == 0xB5) return '/';
+	if(scancode == 0x29) return '0';
+	if(scancode >= 0x2 && scancode <= 0xA)
+		return "123456789"[scancode - 0x2];
+	if(scancode >= 0x10 && scancode <= 0x1C)
+	{
+		return "qwertzuiop"[scancode - 0x10];
+	} else if(scancode >= 0x1E && scancode <= 0x26)
+	{
+		return "asdfghjkl"[scancode - 0x1E];
+	} else if(scancode >= 0x2C && scancode <= 0x32)
+	{
+		return "yxcvbnm"[scancode - 0x2C];
+	}
+	return 0;
 }
