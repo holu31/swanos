a7c056b2ad126d45675642f873c148ac09b7dc6b
Daniil Lebedev <93409280+holu31@users.noreply.github.com>
2022-06-22 19:25:50 +0300

    add first command and fix bug

--- src/io/shell.c
+++ src/io/shell.c
@@ -4,16 +4,18 @@
 #include <string.h>
 #define SIZE_BUFFER 80
 
+char buffer_shell[SIZE_BUFFER];
+
 char *get_command(){
-    char buffer_shell[SIZE_BUFFER];
     int key = kb_getkey();
     cset_color(COLOR_CYAN, COLOR_BLACK);
     cputs("\n~$ ");
     cset_color(COLOR_LIGHT_GREY, COLOR_BLACK);
+    for(int i=0; i<sizeof(buffer_shell); i++) buffer_shell[i] = '\0';
     while(kb_sctochar(key) != '\n'){
         if(kb_sctochar(key) != 0){
-            buffer_shell[sizeof(buffer_shell)] = kb_sctochar(key);
-            cputch(buffer_shell[sizeof(buffer_shell)]);
+            buffer_shell[strlen(buffer_shell)] = kb_sctochar(key);
+            cputch(kb_sctochar(key));
         }
         key = kb_getkey();
     }
@@ -23,5 +25,9 @@ char *get_command(){
 void shell_init(){
     while(1){
         char *command = get_command(); // requires a memory manager in order to work properly
+        if(strcmp(command, "help")==0){
+            cputs("\nhere all commands are output inside in the kernel: \n\
+help - all commands are output inside to the kernel\n");
+        }
     }
 }
