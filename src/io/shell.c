6a7cc8018c20cd3fa096b52afe26792b569c19b7
Daniil Lebedev <93409280+holu31@users.noreply.github.com>
2022-06-24 17:00:37 +0300

    add command for shutdown and reboot computer

--- src/io/shell.c
+++ src/io/shell.c
@@ -1,6 +1,7 @@
 #include <shell.h>
 #include <keyboard.h>
 #include <console.h>
+#include <ports.h>
 #include <string.h>
 #define SIZE_BUFFER 77
 
@@ -14,7 +15,7 @@ char *get_command(){
     for(int i=0; i!=sizeof(buffer_shell); i++) buffer_shell[i] = '\0';
     while(kb_sctochar(key) != '\n'){
         if(strlen(buffer_shell) == SIZE_BUFFER){
-            log("buffer full\n", false);    // temporary buffer overflow protection until the memory manager appears
+            log("buffer full\n", false);
             return "";
         }
         if(kb_sctochar(key) != 0){
@@ -32,6 +33,15 @@ void shell_init(){
         if(strcmp(command, "help")==0){
             cputs("\nhere all commands are output inside in the kernel: \n\
 help - all commands are output inside to the kernel\n");
+        } else if(strcmp(command, "reboot")==0){
+            uint8_t good = 0x02;
+            while(good & 0x02) good = inb(0x64);
+            outb(0x64, 0xFE);
+            asm volatile("hlt");
+        } else if(strcmp(command, "shutdown")==0){
+            outw(0xB004, 0x2000);
+            outw(0x604, 0x2000);
+            outw(0x4004, 0x3400);
         }
     }
 }
