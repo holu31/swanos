dd780825f965dcc65b4301739337449cf0a16551
Daniil Lebedev <93409280+holu31@users.noreply.github.com>
2022-06-26 18:57:08 +0300

    delete mm

--- build-actions.sh
+++ build-actions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OBJECTS="bin/kernel.o bin/kernel_entry.o bin/console.o bin/ports.o bin/string.o bin/gdt.o bin/idt.o bin/kheap.o bin/interrupts.o bin/pci.o bin/isr.o bin/irq.o bin/keyboard.o bin/shell.o bin/description_tables.o"
+OBJECTS="bin/kernel.o bin/kernel_entry.o bin/console.o bin/ports.o bin/string.o bin/gdt.o bin/idt.o bin/interrupts.o bin/pci.o bin/isr.o bin/irq.o bin/keyboard.o bin/shell.o bin/description_tables.o"
 
 i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/io/ports.c -o bin/ports.o
 i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/io/console.c -o bin/console.o
@@ -18,8 +18,6 @@ nasm src/interrupts/interrupts.asm -f elf32 -O0 -o bin/interrupts.o
 i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/drivers/pci.c -o bin/pci.o
 i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/drivers/keyboard.c -o bin/keyboard.o
 i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/io/shell.c -o bin/shell.o
-
-i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/mem/kheap.c -o bin/kheap.o
 
 i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/kernel.c -o bin/kernel.o
 
