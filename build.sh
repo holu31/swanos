b72251c3dabb9485c2f0d4b091020d003ea8ca58
Daniil Lebedev <93409280+holu31@users.noreply.github.com>
2022-06-28 21:09:00 +0300

    optimizer

--- build.sh
+++ build.sh
@@ -15,33 +15,33 @@ if [ ! -x "$(command -v git)" ]; then
   exit
 fi
 
-i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/io/ports.c -o bin/ports.o
-i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/io/console.c -o bin/console.o
+i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O2 -c src/io/ports.c -o bin/ports.o
+i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O2 -c src/io/console.c -o bin/console.o
 
-i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/libc/string.c -o bin/string.o
+i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O2 -c src/libc/string.c -o bin/string.o
 
-i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/arch/x86/gdt.c -o bin/gdt.o
-i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/arch/x86/idt.c -o bin/idt.o
+i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O2 -c src/arch/x86/gdt.c -o bin/gdt.o
+i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O2 -c src/arch/x86/idt.c -o bin/idt.o
 i686-elf-as src/arch/x86/description_tables.s -o bin/description_tables.o
 
-i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/interrupts/irq.c -o bin/irq.o
-i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/interrupts/isr.c -o bin/isr.o
+i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O2 -c src/interrupts/irq.c -o bin/irq.o
+i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O2 -c src/interrupts/isr.c -o bin/isr.o
 nasm src/interrupts/interrupts.asm -f elf32 -O0 -o bin/interrupts.o
 
-i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/drivers/pci.c -o bin/pci.o
-i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/drivers/keyboard.c -o bin/keyboard.o
-i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/io/shell.c -o bin/shell.o
+i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O2 -c src/drivers/pci.c -o bin/pci.o
+i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O2 -c src/drivers/keyboard.c -o bin/keyboard.o
+i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O2 -c src/io/shell.c -o bin/shell.o
 
-i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -c src/kernel.c -o bin/kernel.o
+i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O2 -c src/kernel.c -o bin/kernel.o
 
 i686-elf-as src/kernel_entry.s -o bin/kernel_entry.o
-i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O0 -nostdlib -lgcc -T link.ld -o build/boot/kernel.elf $OBJECTS
+i686-elf-gcc -g -I include -ffreestanding -Wall -Wextra -O2 -nostdlib -lgcc -T link.ld -o build/boot/kernel.elf $OBJECTS
 
 if [ "$1" == "grub" ]; then
   if [ -x "$(command -v grub-mkrescue)" ]; then
-    grub2-mkrescue -o "swanos-latest.iso" build/ -V SwanOS
+    grub-mkrescue -o "swanos-latest.iso" build/ -V SwanOS
   elif [ -x "$(command -v grub2-mkrescue)" ]; then
-    grub2-mkrescue -o "swanos-latest.iso" build/ -V SwanOS
+    grub-mkrescue -o "swanos-latest.iso" build/ -V SwanOS
   else
     echo "ERROR: grub not installed!"
     exit
